32e720f39607c0fea9d62c96c8bcb4b46074203b
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-11-09 00:03:33 -0300

    :sparkles: feat: sendPasswordResetEmail

--- src/main/java/live/bolaocopadomundo/api/services/email/EmailService.java
+++ src/main/java/live/bolaocopadomundo/api/services/email/EmailService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,9 @@ public class EmailService {
     @Value("${mail.from}")
     private String emailFrom;
 
+    @Value("${url}")
+    private String url;
+
     @Autowired
     private JavaMailSender emailSender;
 
@@ -21,6 +25,7 @@ public class EmailService {
         this.emailSender.send(simpleMailMessage);
     }
 
+    @Async
     public void sendConfirmationEmail(User user) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(emailFrom);
@@ -29,6 +34,20 @@ public class EmailService {
         message.setText("Olá, "
                 + user.getNickname() + "!\n\n"
                 + "Seu código de ativação é: " + user.getActivationCode() + "\n\n"
+                + "Atenciosamente,\n" + "Bolão Copa do Mundo 2022");
+
+        this.sendMailMessage(message);
+    }
+
+    @Async
+    public void sendPasswordResetEmail(User user, String token) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(emailFrom);
+        message.setTo(user.getEmail());
+        message.setSubject("Bolão Copa do Mundo 2022 - Redefinição de Senha");
+        message.setText("Olá, "
+                + user.getNickname() + "!\n\n"
+                + "Aqui está o link para mudar sua senha:\n" + url + "/auth/password-reset/" + token + "\n\n"
                 + "Atenciosamente,\n" + "Bolão Copa do Mundo 2022");
 
         this.sendMailMessage(message);
